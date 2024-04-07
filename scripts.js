1c6aca06967550dfb453fe3420d7bc1904ffb843
Myat-Thiha <mthiha@go.pasadena.edu>
2024-04-07 16:35:19 -0700

    can know clicked on the albums to get to the songs page and have back button to back to albums page

--- scripts.js
+++ scripts.js
@@ -26,44 +26,6 @@
 
 // This is an array of strings (TV show titles)
 
-// let Album1songs = [
-//     {"Name": "May Lite Tot", "Duration": "3:43"},
-//     {"Name": "Yat Sat Swar Pyone Tat Thaw", "Duration": "4:28"},
-//     {"Name": "Khwint Ma Pyu", "Duration": "4:34"},
-//     {"Name": "Nar Yi Paw Ka Myat Yay Sat Myar", "Duration": "3:17"},
-//     {"Name": "Sate Dat Kin Sar", "Duration": "3:33"},
-//     {"Name": "Ta Naye Naye Tot Chit Lar Laint Ml", "Duration": "3:50"},
-//     {"Name": "Kyite Tha Lo Lote", "Duration": "4:03"},
-//     {"Name": "Ma Yoe Tot Buu", "Duration": "2:31"},
-//     {"Name": "Sway Pyi", "Duration": "2:59"},
-//     {"Name": "Thi Chin Lay Thet Thay", "Duration": "3:52"},
-//     {"Name": "Thu Nge Chin Atweet", "Duration": "Unknown"},
-//     {"Name": "Way Thwar Tae A Khar", "Duration": "5:11"},
-//     {"Name": "Nar Yi Yae Thichin", "Duration": "3:45"},
-//     {"Name": "Chit", "Duration": "2:37"},
-//     {"Name": "Nyi Lay Yay", "Duration": "4:10"},
-//     {"Name": "Sain Yat Lay Arr", "Duration": "Unknown"}
-// ];
-
-// let titles = [
-//     {"Name":"Naryi Baw Mha Myet Yay Set Myar -Tear Drops on the Clock", "year":1986, "listened":1468, "image": "https://i.scdn.co/image/ab67616d0000b2736ec10c07236cf35fa44db682", "songs":[...Album1songs]},
-//     {"Name": "Mhaw Sayar Eainmet - Dream of a Wizard", "year":1988, "listened":1207, "image": "https://i.ytimg.com/vi/FDm3s2d23jQ/maxresdefault.jpg"},
-//     {"Name": "Thet Nyein - Tranquillity", "year":1989, "listened":1450, "image": "https://i.ytimg.com/vi/-a6LFPAz_24/hqdefault.jpg"},
-//     {"Name": "Atta Pon Saung Khae Myar - Crystal Of Ego", "year":1991, "listened":2340, "image": "https://th.bing.com/th/id/OIP.Hs15ORkP-QkZevIkYIEirAAAAA?rs=1&pid=ImgDetMain"},
-//     {"Name": "Akyinna Eainmet Dream of Affection", "year":1991, "listened":7840, "image": "https://i.scdn.co/image/ab67616d0000b273aa96c90c62f988a4a6bbe86e"},
-//     {"Name": "A Yin A Taing - Just Like Old Times", "year":1991, "listened":8145, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/39/d5/a5/39d5a522-d09f-e1a5-5101-4ca65c19aa41/8720205504111.png/1200x1200bf-60.jpg"},
-//     {"Name": "Zaga Lon Ma Shi Tae Kaungkin - Sky Without Words", "year":1995, "listened":1408, "image": "https://th.bing.com/th/id/OIP.8oMhFKKqKeUzibIuo8zlmgAAAA?rs=1&pid=ImgDetMain"},
-//     {"Name": "Htoo Eain Thin Unplugged Live", "year":1996, "listened":5862, "image": "https://i.ytimg.com/vi/yM8F0zjwhLU/maxresdefault.jpg"},
-//     {"Name": "Myo Pya La Yaung Tan Chin", "year":1996, "listened":4371, "image": "https://i.scdn.co/image/ab67616d0000b2735f40a9df186bbd7597d95f53"},
-//     {"Name": "Win Yoe Sune Mee - Polar Flame", "year":1997, "listened":5479, "image": "https://i.scdn.co/image/ab67616d0000b2739de7a63da706368c28902cac"},
-//     {"Name": "A May (Tho) A Kywin Mae Metta Taw Thi Chin - Mother (or) Owner Of Endless Love", "year":1997, "listened":3560, "image": "https://www.layeik.com/wp-content/uploads/edd/2017/06/Htoo-Eain-Thin-A-May-768x768.jpg"},
-//     {"Name": "A Mhat Ta Ya Dan Ya Thi Chin Mya - Songs For Memorial Scars Of Life", "year":1999, "listened":8945, "image": "https://4.bp.blogspot.com/_qzszAXMm3BY/SA2Jmqx-4YI/AAAAAAAAAfc/ZAYE81R1St4/s400/HTOO+11.JPG"},
-//     {"Name": "A Sein Yaung Yet Swe Myar - Green Dates ", "year":2001, "listened":7690, "image": "https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/59/77/c4/5977c443-41f2-af15-c9b9-b509711ebf8c/8720205504081.png/1200x1200bf-60.jpg"},
-//     {"Name": "Ta Nae Sar A Lwe Myar - Mistakes of the Day", "year":2003, "listened":8469, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9b/c0/4e/9bc04e48-91a7-d6ea-4e9a-4d34566c5850/8720205504029.png/1200x1200bf-60.jpg"},
-//     {"Name": "Chit Chin Ah Phyint - With Love", "year":2005, "listened":4267, "image": "https://th.bing.com/th/id/OIP.nyFIDh47jyFqh9aZ0v4EUAAAAA?rs=1&pid=ImgDetMain"},
-//     {"Name": "Nhit Cho' Waing", "year":"(unreleased)", "listened":0, "image": "https://www.layeik.com/wp-content/uploads/Htoo-Eain-Thin-500x500.jpg"}
-// ];
-
 import titles from './titles.js';
 
 let getTitles = titles;
@@ -165,6 +127,13 @@ export function ls_6000_10000()
 }
 window.ls_6000_10000 = ls_6000_10000;
 
+export function back()
+{
+    document.getElementById("card-container").style.display = "block";
+    document.getElementById("song-container").style.display = "none";
+}
+window.back = back;
+
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
@@ -179,7 +148,30 @@ function showCards() {
         const nextCard = templateCard.cloneNode(true); // Copy the template card
         editCardContent(nextCard, title, title.image); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
+        nextCard.addEventListener("click", function() {
+            // Navigate to the song page for the clicked album
+            navigateToSongPage(title);
+        });
+    }
+}
 
+function navigateToSongPage(album) {
+    // Hide the card container and show the song container
+    document.getElementById("card-container").style.display = "none";
+    document.getElementById("song-container").style.display = "block";
+    // Set the album title on the song page
+    document.getElementById("album-title").textContent = album.songs.Name;
+
+    // Get the song list container
+    const songList = document.getElementById("song-list");
+    songList.innerHTML = ""; // Clear previous song list
+
+    // Add songs of the clicked album to the song list
+    for (let song of album.songs) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${song.Name} - ${song.Duration}`;
+        songList.appendChild(listItem);
+        console.log("clicked");
     }
 }
 
--- titles.js
+++ titles.js
@@ -19,21 +19,21 @@ let Album1songs = [
 
 const titles = [
     {"Name":"Naryi Baw Mha Myet Yay Set Myar -Tear Drops on the Clock", "year":1986, "listened":1468, "image": "https://i.scdn.co/image/ab67616d0000b2736ec10c07236cf35fa44db682", "songs":[...Album1songs]},
-    {"Name": "Mhaw Sayar Eainmet - Dream of a Wizard", "year":1988, "listened":1207, "image": "https://i.ytimg.com/vi/FDm3s2d23jQ/maxresdefault.jpg"},
-    {"Name": "Thet Nyein - Tranquillity", "year":1989, "listened":1450, "image": "https://i.ytimg.com/vi/-a6LFPAz_24/hqdefault.jpg"},
-    {"Name": "Atta Pon Saung Khae Myar - Crystal Of Ego", "year":1991, "listened":2340, "image": "https://th.bing.com/th/id/OIP.Hs15ORkP-QkZevIkYIEirAAAAA?rs=1&pid=ImgDetMain"},
-    {"Name": "Akyinna Eainmet Dream of Affection", "year":1991, "listened":7840, "image": "https://i.scdn.co/image/ab67616d0000b273aa96c90c62f988a4a6bbe86e"},
-    {"Name": "A Yin A Taing - Just Like Old Times", "year":1991, "listened":8145, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/39/d5/a5/39d5a522-d09f-e1a5-5101-4ca65c19aa41/8720205504111.png/1200x1200bf-60.jpg"},
-    {"Name": "Zaga Lon Ma Shi Tae Kaungkin - Sky Without Words", "year":1995, "listened":1408, "image": "https://th.bing.com/th/id/OIP.8oMhFKKqKeUzibIuo8zlmgAAAA?rs=1&pid=ImgDetMain"},
-    {"Name": "Htoo Eain Thin Unplugged Live", "year":1996, "listened":5862, "image": "https://i.ytimg.com/vi/yM8F0zjwhLU/maxresdefault.jpg"},
-    {"Name": "Myo Pya La Yaung Tan Chin", "year":1996, "listened":4371, "image": "https://i.scdn.co/image/ab67616d0000b2735f40a9df186bbd7597d95f53"},
-    {"Name": "Win Yoe Sune Mee - Polar Flame", "year":1997, "listened":5479, "image": "https://i.scdn.co/image/ab67616d0000b2739de7a63da706368c28902cac"},
-    {"Name": "A May (Tho) A Kywin Mae Metta Taw Thi Chin - Mother (or) Owner Of Endless Love", "year":1997, "listened":3560, "image": "https://www.layeik.com/wp-content/uploads/edd/2017/06/Htoo-Eain-Thin-A-May-768x768.jpg"},
-    {"Name": "A Mhat Ta Ya Dan Ya Thi Chin Mya - Songs For Memorial Scars Of Life", "year":1999, "listened":8945, "image": "https://4.bp.blogspot.com/_qzszAXMm3BY/SA2Jmqx-4YI/AAAAAAAAAfc/ZAYE81R1St4/s400/HTOO+11.JPG"},
-    {"Name": "A Sein Yaung Yet Swe Myar - Green Dates ", "year":2001, "listened":7690, "image": "https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/59/77/c4/5977c443-41f2-af15-c9b9-b509711ebf8c/8720205504081.png/1200x1200bf-60.jpg"},
-    {"Name": "Ta Nae Sar A Lwe Myar - Mistakes of the Day", "year":2003, "listened":8469, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9b/c0/4e/9bc04e48-91a7-d6ea-4e9a-4d34566c5850/8720205504029.png/1200x1200bf-60.jpg"},
-    {"Name": "Chit Chin Ah Phyint - With Love", "year":2005, "listened":4267, "image": "https://th.bing.com/th/id/OIP.nyFIDh47jyFqh9aZ0v4EUAAAAA?rs=1&pid=ImgDetMain"},
-    {"Name": "Nhit Cho' Waing", "year":"(unreleased)", "listened":0, "image": "https://www.layeik.com/wp-content/uploads/Htoo-Eain-Thin-500x500.jpg"}
+    {"Name": "Mhaw Sayar Eainmet - Dream of a Wizard", "year":1988, "listened":1207, "image": "https://i.ytimg.com/vi/FDm3s2d23jQ/maxresdefault.jpg", "songs":[...Album1songs]},
+    {"Name": "Thet Nyein - Tranquillity", "year":1989, "listened":1450, "image": "https://i.ytimg.com/vi/-a6LFPAz_24/hqdefault.jpg", "songs":[...Album1songs]},
+    {"Name": "Atta Pon Saung Khae Myar - Crystal Of Ego", "year":1991, "listened":2340, "image": "https://th.bing.com/th/id/OIP.Hs15ORkP-QkZevIkYIEirAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
+    {"Name": "Akyinna Eainmet Dream of Affection", "year":1991, "listened":7840, "image": "https://i.scdn.co/image/ab67616d0000b273aa96c90c62f988a4a6bbe86e", "songs":[...Album1songs]},
+    {"Name": "A Yin A Taing - Just Like Old Times", "year":1991, "listened":8145, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/39/d5/a5/39d5a522-d09f-e1a5-5101-4ca65c19aa41/8720205504111.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
+    {"Name": "Zaga Lon Ma Shi Tae Kaungkin - Sky Without Words", "year":1995, "listened":1408, "image": "https://th.bing.com/th/id/OIP.8oMhFKKqKeUzibIuo8zlmgAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
+    {"Name": "Htoo Eain Thin Unplugged Live", "year":1996, "listened":5862, "image": "https://i.ytimg.com/vi/yM8F0zjwhLU/maxresdefault.jpg", "songs":[...Album1songs]},
+    {"Name": "Myo Pya La Yaung Tan Chin", "year":1996, "listened":4371, "image": "https://i.scdn.co/image/ab67616d0000b2735f40a9df186bbd7597d95f53", "songs":[...Album1songs]},
+    {"Name": "Win Yoe Sune Mee - Polar Flame", "year":1997, "listened":5479, "image": "https://i.scdn.co/image/ab67616d0000b2739de7a63da706368c28902cac", "songs":[...Album1songs]},
+    {"Name": "A May (Tho) A Kywin Mae Metta Taw Thi Chin - Mother (or) Owner Of Endless Love", "year":1997, "listened":3560, "image": "https://www.layeik.com/wp-content/uploads/edd/2017/06/Htoo-Eain-Thin-A-May-768x768.jpg", "songs":[...Album1songs]},
+    {"Name": "A Mhat Ta Ya Dan Ya Thi Chin Mya - Songs For Memorial Scars Of Life", "year":1999, "listened":8945, "image": "https://4.bp.blogspot.com/_qzszAXMm3BY/SA2Jmqx-4YI/AAAAAAAAAfc/ZAYE81R1St4/s400/HTOO+11.JPG", "songs":[...Album1songs]},
+    {"Name": "A Sein Yaung Yet Swe Myar - Green Dates ", "year":2001, "listened":7690, "image": "https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/59/77/c4/5977c443-41f2-af15-c9b9-b509711ebf8c/8720205504081.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
+    {"Name": "Ta Nae Sar A Lwe Myar - Mistakes of the Day", "year":2003, "listened":8469, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9b/c0/4e/9bc04e48-91a7-d6ea-4e9a-4d34566c5850/8720205504029.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
+    {"Name": "Chit Chin Ah Phyint - With Love", "year":2005, "listened":4267, "image": "https://th.bing.com/th/id/OIP.nyFIDh47jyFqh9aZ0v4EUAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
+    {"Name": "Nhit Cho' Waing", "year":"(unreleased)", "listened":0, "image": "https://www.layeik.com/wp-content/uploads/Htoo-Eain-Thin-500x500.jpg", "songs":[...Album1songs]}
 ];
 
 export default titles;
