2dbeb6f13ffc042697b040e479efa06e9da4c764
Myat-Thiha <mthiha@go.pasadena.edu>
2024-04-07 23:14:34 -0700

    all songs added and linked to respective albums

--- titles.js
+++ titles.js
@@ -9,31 +9,242 @@ let Album1songs = [
     {"Name": "Ma Yoe Tot Buu", "Duration": "2:31"},
     {"Name": "Sway Pyi", "Duration": "2:59"},
     {"Name": "Thi Chin Lay Thet Thay", "Duration": "3:52"},
-    {"Name": "Thu Nge Chin Atweet", "Duration": "Unknown"},
+    {"Name": "Thu Nge Chin Atweet", "Duration": "4:15"},
     {"Name": "Way Thwar Tae A Khar", "Duration": "5:11"},
     {"Name": "Nar Yi Yae Thichin", "Duration": "3:45"},
     {"Name": "Chit", "Duration": "2:37"},
     {"Name": "Nyi Lay Yay", "Duration": "4:10"},
-    {"Name": "Sain Yat Lay Arr", "Duration": "Unknown"}
+    {"Name": "Sain Yat Lay Arr", "Duration": "3:24"}
 ];
+
+let Album2songs = [
+    {"Name": "A Chit Sone Thot", "Duration": "5:18"},
+    {"Name": "Nat Mi Mal", "Duration": "3:36"},
+    {"Name": "A Maye Eain", "Duration": "4:04"},
+    {"Name": "Myat Yay Wel Yone", "Duration": "3:53"},
+    {"Name": "Chit Tal Haye", "Duration": "2:44"},
+    {"Name": "Kyoe Wine", "Duration": "3:34"},
+    {"Name": "Mhyaw Lint Chat Thar Ma Htar Khae Yin", "Duration": "3:54"},
+    {"Name": "Yat Sat Lite Tar", "Duration": "4:06"},
+    {"Name": "Thin Kwel Nhat", "Duration": "3:39"},
+    {"Name": "Virus Poe", "Duration": "2:35"},
+    {"Name": "Soe Yain Loh", "Duration": "3:33"},
+    {"Name": "Nyi Lay Yay 2", "Duration": "2:55"},
+    {"Name": "Thwar Kwal", "Duration": "3:28"},
+    {"Name": "A Yuu A Muu", "Duration": "2:48"},
+    {"Name": "Ma Shi", "Duration": "4:14"},
+    {"Name": "Pyaw Mal So", "Duration": "3:42"}
+];
+
+let Album3songs = [
+    {"Name": "Hna Lone Thar Yae Kyan Sar", "Duration": "3:25"},
+    {"Name": "Khaung Laung Lay Tway Myi Nay Pyi", "Duration": "3:38"},
+    {"Name": "A Kyin Nar Kin Tae Main Ma Hla", "Duration": "4:39"},
+    {"Name": "Chit Tine Lae Ma Nyar", "Duration": "4:21"},
+    {"Name": "Ka Lay Yay", "Duration": "4:00"},
+    {"Name": "A Lo At Sone", "Duration": "4:38"},
+    {"Name": "Lu Nae Ingyi", "Duration": "3:34"},
+    {"Name": "Thet Nyein", "Duration": "4:15"},
+    {"Name": "Nyi Ma Lay", "Duration": "3:06"},
+    {"Name": "A Yate Mae", "Duration": "3:30"},
+    {"Name": "Chit Thu Way", "Duration": "4:26"},
+    {"Name": "Mhar Khae Tal", "Duration": "3:25"},
+    {"Name": "Ku Nyi Chin Tal", "Duration": "4:13"},
+    {"Name": "Kar Yan Ma Nyi Thaw", "Duration": "4:41"},
+    {"Name": "Nyein Chan Par Say", "Duration": "5:12"}
+];
+
+let Album4songs = [
+    {"Name": "Sway Tal", "Duration": "3:21"},
+    {"Name": "Ta Say A Nan", "Duration": "3:43"},
+    {"Name": "Chit Thu Lat Saung", "Duration": "4:17"},
+    {"Name": "Yarzawin Myar Yae Tha Toh Tha Mee", "Duration": "4:57"},
+    {"Name": "Jatsin Htate Ka La Yate Pyar", "Duration": "4:46"},
+    {"Name": "Rock In Roll A Pyo Ma", "Duration": "4:05"},
+    {"Name": "A Khar Lon Tae Moe", "Duration": "4:37"},
+    {"Name": "Yin Khone Yin A Chit", "Duration": "5:57"},
+    {"Name": "A Myae Yae Dotekha Oh Lay", "Duration": "4:58"},
+    {"Name": "Lwan Thu Eain Mat", "Duration": "5:23"},
+    {"Name": "A Sain Yaung Takhar Myar", "Duration": "3:34"},
+    {"Name": "Myoh Pya Nya Myar", "Duration": "5:07"},
+    {"Name": "Min Ma Shi Tae Nout", "Duration": "4:50"}
+];
+
+let Album5songs = [
+    {"Name": "A Kyin Nar Eain Mat", "Duration": "4:42"},
+    {"Name": "A Myae Eain", "Duration": "4:10"},
+    {"Name": "Sain Yat Lay Arr", "Duration": "4:33"},
+    {"Name": "Ma Sone Tae Ta Chin", "Duration": "4:41"},
+    {"Name": "Thet Nyein", "Duration": "4:15"}
+];
+
+let Album6songs= [
+    {"Name": "Shwe", "Duration": "3:02"},
+    {"Name": "A Htee Kyan", "Duration": "3:20"},
+    {"Name": "Min Shi Yin Phyit Tal", "Duration": "3:00"},
+    {"Name": "Vinn Sint Van Goe Tann Chin", "Duration": "4:15"},
+    {"Name": "Htar Wa Sin", "Duration": "3:50"},
+    {"Name": "A Chit A Kyaung", "Duration": "3:13"},
+    {"Name": "Thwar", "Duration": "3:25"},
+    {"Name": "Dotekha Myit Myar", "Duration": "4:57"},
+    {"Name": "A Yin A Tine", "Duration": "4:36"},
+    {"Name": "Eain Mat", "Duration": "4:51"},
+    {"Name": "Hlotwat A Chit", "Duration": "4:07"},
+    {"Name": "A Sone A Phyat", "Duration": "3:48"},
+    {"Name": "Min Si A Pyay", "Duration": "3:39"},
+    {"Name": "Ta Kal So Yin", "Duration": "3:04"},
+    {"Name": "Thu Sain San San", "Duration": "4:17"}
+];
+
+let Album7songs = [
+    {"Name": "A Chit Nat Soe", "Duration": "3:56"},
+    {"Name": "A Lwan Yae Mee Hlyan Myar", "Duration": "3:54"},
+    {"Name": "Dan Yar Myar Paw Ka Pyay Lann", "Duration": "3:22"},
+    {"Name": "Kaung Kin Htae Ka Myit Ta Sin", "Duration": "4:50"},
+    {"Name": "Yin Htae Ka Moe Sat Myar Lay Phyu", "Duration": "3:30"},
+    {"Name": "A Chit Yae Lat Myar Nae", "Duration": "5:09"},
+    {"Name": "Ta Kal Loh Myar", "Duration": "2:32"},
+    {"Name": "Cho Thaw A Shone Myar", "Duration": "4:03"},
+    {"Name": "Sakar Lone Ma Shi Tae Kaung Kin", "Duration": "3:41"},
+    {"Name": "A Thel Kwel Khae Tae Kabar", "Duration": "4:40"},
+    {"Name": "A Mone Nae Yay Tae Thi Chin Ta Pote", "Duration": "3:56"},
+    {"Name": "Than Khaung Yan A Sway Myar", "Duration": "5:01"},
+    {"Name": "Dandaryi Ta Khu", "Duration": "4:08"},
+    {"Name": "Chit Thu Yae Myit", "Duration": "4:34"}
+];
+
+let Album8songs = [
+    {"Name": "A Lo At Sone", "Duration": "4:38"},
+    {"Name": "Khwint Ma Pyu", "Duration": "4:34"},
+    {"Name": "Naryi Paw Ka Myat Yay Sat Myar", "Duration": "4:27"},
+    {"Name": "Nyi Ma Lay", "Duration": "3:06"},
+    {"Name": "Koh Yae Nya Myar", "Duration": "4:27"},
+    {"Name": "Sain Yat Lay Arr", "Duration": "4:33"},
+    {"Name": "A Maye Eain", "Duration": "4:04"},
+    {"Name": "A Maye Yae Dote Kha Oh Lay", "Duration": "5:40"},
+    {"Name": "Rock In Roll A Pyo Ma", "Duration": "4:05"},
+    {"Name": "Lwan Thu Eain Mat", "Duration": "5:23"},
+    {"Name": "Thwar Kwal", "Duration": "3:28"},
+    {"Name": "Way Thwar Tae A Khar", "Duration": "5:11"}
+];
+
+let Album9songs = [
+    {"Name": "Lay Lwint Chin Lann Ma Myar", "Duration": "4:15"},
+    {"Name": "Zat Thein A Lwan Khan", "Duration": "5:03"},
+    {"Name": "Eain Mat Htae Ka Hnote Khan Lay", "Duration": "5:41"},
+    {"Name": "Eain Pyan Chain Myar", "Duration": "4:02"},
+    {"Name": "Ta Kal So Min Ka", "Duration": "4:47"},
+    {"Name": "Myoh Pya La Yaung Tann Chin", "Duration": "5:30"}
+];
+
+let Album10songs = [
+    {"Name": "Kyain Sar Thint Yin Kwin", "Duration": "4:05"},
+    {"Name": "Sagar Lone Tway Thate Ma Lo Buu", "Duration": "4:36"},
+    {"Name": "Win Yoe Soon Mee", "Duration": "4:30"},
+    {"Name": "Way Lwint Chin", "Duration": "5:19"},
+    {"Name": "Thu A Mone", "Duration": "5:49"},
+    {"Name": "Htar Yit Thu Thwar Khae Yin", "Duration": "4:00"},
+    {"Name": "Nhit Yout A Chit", "Duration": "4:27"},
+    {"Name": "Honey", "Duration": "3:18"},
+    {"Name": "A Sin A Myal", "Duration": "4:37"},
+    {"Name": "Nout Sone A Nan", "Duration": "4:56"}
+];
+
+let Album11songs = [
+    {"Name": "May May Chit Tae Tharr Gyi", "Duration": "3:16"},
+    {"Name": "Kyway Haung Sat Khwint Pyu Par A May", "Duration": "4:40"},
+    {"Name": "A Maye Yae Dotekha Oh Lay", "Duration": "5:02"},
+    {"Name": "A Maye Eain", "Duration": "4:04"},
+    {"Name": "A May Ta Thu Thar Ta Khu", "Duration": "4:28"},
+    {"Name": "May May Nay Kaung Lar", "Duration": "2:44"},
+    {"Name": "A Yate Sit", "Duration": "4:43"},
+    {"Name": "Kyae Zuu Par May May", "Duration": "2:34"},
+    {"Name": "A Maye Aye Yate", "Duration": "4:50"},
+    {"Name": "May May", "Duration": "4:22"},
+    {"Name": "A May", "Duration": "4:47"}
+];
+
+let Album12songs = [
+    {"Name": "Sakar Lone Ma Shi Tae Kaung Kin (2)", "Duration": "3:25"},
+    {"Name": "A Mhat Taya Danyar", "Duration": "4:49"},
+    {"Name": "Kaung Kin Htae Ka Myit Ta Sin", "Duration": "4:50"},
+    {"Name": "A Htee Kyan", "Duration": "3:20"},
+    {"Name": "A Lwan Yae Mee Hlyan Myar", "Duration": "3:54"},
+    {"Name": "Eain", "Duration": "3:59"},
+    {"Name": "Htar Wa Sin", "Duration": "3:50"},
+    {"Name": "Shwe Sin", "Duration": "4:11"},
+    {"Name": "Thwar", "Duration": "3:25"},
+    {"Name": "Nyi Mya Chin", "Duration": "3:11"},
+    {"Name": "Cho Thaw A Shone Myar", "Duration": "4:03"},
+    {"Name": "Vinn Sint Van Goe Tann Chin", "Duration": "4:15"}
+];
+
+
+let Album13songs = [
+    {"Name": "Nya Pone Pyin", "Duration": "4:29"},
+    {"Name": "A Mone", "Duration": "3:47"},
+    {"Name": "Pyan Ma Lar Tot Buu Kwel", "Duration": "4:22"},
+    {"Name": "Yu Yu Mite Mite", "Duration": "4:23"},
+    {"Name": "A Sain Yaung Yat Swal Myar", "Duration": "4:23"},
+    {"Name": "A Mhar A Ywin Ka Pwel", "Duration": "4:06"},
+    {"Name": "Ma Har Wi Ya O Dotekha", "Duration": "5:01"},
+    {"Name": "Ma Sone Naing Khin", "Duration": "4:18"},
+    {"Name": "Pari Yal", "Duration": "4:53"},
+    {"Name": "Lan Pyout Tae Thar", "Duration": "5:17"},
+    {"Name": "Nar Yi Tway Yat Tae Nya", "Duration": "4:29"},
+    {"Name": "A Way Sone Way Thwar Lae", "Duration": "4:41"}
+];
+
+let Album14songs = [
+    {"Name": "Ta Naye Sar A Lwae Myar", "Duration": "5:00"},
+    {"Name": "Ta Thet Tar A Mhat Ta Ya", "Duration": "4:04"},
+    {"Name": "A Di Pa Di Lann Ka Chay Yar Myar", "Duration": "4:44"},
+    {"Name": "Phyay Phyay Lay Nae Nyeint Nyeint Lay", "Duration": "3:59"},
+    {"Name": "Dotekha Ko Say Soe Htar Tal", "Duration": "3:30"},
+    {"Name": "A Mone Myat Lone", "Duration": "4:13"},
+    {"Name": "Nway Oo Kant Kaw", "Duration": "2:58"},
+    {"Name": "Nout Htet Ma Shi", "Duration": "3:39"},
+    {"Name": "Hnote Sat Mae Nway", "Duration": "3:44"},
+    {"Name": "Thone Yar Thi Chit Thu", "Duration": "5:34"},
+    {"Name": "24 Hours", "Duration": "3:20"},
+    {"Name": "Phayar Wit Pyu Lay", "Duration": "3:07"},
+    {"Name": "Pa Hta Ma Sone A Thel Kwel Zat Lan", "Duration": "3:33"}
+];
+
+let Album15songs = [
+    {"Name": "Kyal Tway Sone Tae Nya", "Duration": "4:15"},
+    {"Name": "Tu Hnine Mae Yadanar", "Duration": "4:44"},
+    {"Name": "A Linn Saing Yae Thi Chin", "Duration": "4:22"},
+    {"Name": "Toh Nhit Youk Yae Eain", "Duration": "3:41"},
+    {"Name": "Pyu Thu A Thit Phyit Thu A Haung", "Duration": "4:35"},
+    {"Name": "Lu Nae Ingyi", "Duration": "3:34"},
+    {"Name": "Moe Tain Pya Zat", "Duration": "4:20"},
+    {"Name": "Wai Danar", "Duration": "3:51"},
+    {"Name": "Haung Nwan Khae Thaw Naye Yat Lay Myar", "Duration": "5:23"},
+    {"Name": "Nout Sone Thi Chinn", "Duration": "4:10"},
+    {"Name": "Ngar Tayar Si Yin Taw Mu Khan", "Duration": "4:37"},
+    {"Name": "Hnote Sat TaPoy", "Duration": "3:58"}
+];
+
+let Album16songs = [{"Name":"Unreleased" , "Duration": "0:00"}]
 
 const titles = [
     {"Name":"Naryi Baw Mha Myet Yay Set Myar -Tear Drops on the Clock", "year":1986, "listened":1468, "image": "https://i.scdn.co/image/ab67616d0000b2736ec10c07236cf35fa44db682", "songs":[...Album1songs]},
-    {"Name": "Mhaw Sayar Eainmet - Dream of a Wizard", "year":1988, "listened":1207, "image": "https://i.ytimg.com/vi/FDm3s2d23jQ/maxresdefault.jpg", "songs":[...Album1songs]},
-    {"Name": "Thet Nyein - Tranquillity", "year":1989, "listened":1450, "image": "https://i.ytimg.com/vi/-a6LFPAz_24/hqdefault.jpg", "songs":[...Album1songs]},
-    {"Name": "Atta Pon Saung Khae Myar - Crystal Of Ego", "year":1991, "listened":2340, "image": "https://th.bing.com/th/id/OIP.Hs15ORkP-QkZevIkYIEirAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
-    {"Name": "Akyinna Eainmet Dream of Affection", "year":1991, "listened":7840, "image": "https://i.scdn.co/image/ab67616d0000b273aa96c90c62f988a4a6bbe86e", "songs":[...Album1songs]},
-    {"Name": "A Yin A Taing - Just Like Old Times", "year":1991, "listened":8145, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/39/d5/a5/39d5a522-d09f-e1a5-5101-4ca65c19aa41/8720205504111.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
-    {"Name": "Zaga Lon Ma Shi Tae Kaungkin - Sky Without Words", "year":1995, "listened":1408, "image": "https://th.bing.com/th/id/OIP.8oMhFKKqKeUzibIuo8zlmgAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
-    {"Name": "Htoo Eain Thin Unplugged Live", "year":1996, "listened":5862, "image": "https://i.ytimg.com/vi/yM8F0zjwhLU/maxresdefault.jpg", "songs":[...Album1songs]},
-    {"Name": "Myo Pya La Yaung Tan Chin", "year":1996, "listened":4371, "image": "https://i.scdn.co/image/ab67616d0000b2735f40a9df186bbd7597d95f53", "songs":[...Album1songs]},
-    {"Name": "Win Yoe Sune Mee - Polar Flame", "year":1997, "listened":5479, "image": "https://i.scdn.co/image/ab67616d0000b2739de7a63da706368c28902cac", "songs":[...Album1songs]},
-    {"Name": "A May (Tho) A Kywin Mae Metta Taw Thi Chin - Mother (or) Owner Of Endless Love", "year":1997, "listened":3560, "image": "https://www.layeik.com/wp-content/uploads/edd/2017/06/Htoo-Eain-Thin-A-May-768x768.jpg", "songs":[...Album1songs]},
-    {"Name": "A Mhat Ta Ya Dan Ya Thi Chin Mya - Songs For Memorial Scars Of Life", "year":1999, "listened":8945, "image": "https://4.bp.blogspot.com/_qzszAXMm3BY/SA2Jmqx-4YI/AAAAAAAAAfc/ZAYE81R1St4/s400/HTOO+11.JPG", "songs":[...Album1songs]},
-    {"Name": "A Sein Yaung Yet Swe Myar - Green Dates ", "year":2001, "listened":7690, "image": "https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/59/77/c4/5977c443-41f2-af15-c9b9-b509711ebf8c/8720205504081.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
-    {"Name": "Ta Nae Sar A Lwe Myar - Mistakes of the Day", "year":2003, "listened":8469, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9b/c0/4e/9bc04e48-91a7-d6ea-4e9a-4d34566c5850/8720205504029.png/1200x1200bf-60.jpg", "songs":[...Album1songs]},
-    {"Name": "Chit Chin Ah Phyint - With Love", "year":2005, "listened":4267, "image": "https://th.bing.com/th/id/OIP.nyFIDh47jyFqh9aZ0v4EUAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album1songs]},
-    {"Name": "Nhit Cho' Waing", "year":"(unreleased)", "listened":0, "image": "https://www.layeik.com/wp-content/uploads/Htoo-Eain-Thin-500x500.jpg", "songs":[...Album1songs]}
+    {"Name": "Mhaw Sayar Eainmet - Dream of a Wizard", "year":1988, "listened":1207, "image": "https://i.ytimg.com/vi/FDm3s2d23jQ/maxresdefault.jpg", "songs":[...Album2songs]},
+    {"Name": "Thet Nyein - Tranquillity", "year":1989, "listened":1450, "image": "https://i.ytimg.com/vi/-a6LFPAz_24/hqdefault.jpg", "songs":[...Album3songs]},
+    {"Name": "Atta Pon Saung Khae Myar - Crystal Of Ego", "year":1991, "listened":2340, "image": "https://th.bing.com/th/id/OIP.Hs15ORkP-QkZevIkYIEirAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album4songs]},
+    {"Name": "Akyinna Eainmet Dream of Affection", "year":1991, "listened":7840, "image": "https://i.scdn.co/image/ab67616d0000b273aa96c90c62f988a4a6bbe86e", "songs":[...Album5songs]},
+    {"Name": "A Yin A Taing - Just Like Old Times", "year":1991, "listened":8145, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/39/d5/a5/39d5a522-d09f-e1a5-5101-4ca65c19aa41/8720205504111.png/1200x1200bf-60.jpg", "songs":[...Album6songs]},
+    {"Name": "Zaga Lon Ma Shi Tae Kaungkin - Sky Without Words", "year":1995, "listened":1408, "image": "https://th.bing.com/th/id/OIP.8oMhFKKqKeUzibIuo8zlmgAAAA?rs=1&pid=ImgDetMain", "songs":[...Album7songs]},
+    {"Name": "Htoo Eain Thin Unplugged Live", "year":1996, "listened":5862, "image": "https://i.ytimg.com/vi/yM8F0zjwhLU/maxresdefault.jpg", "songs":[...Album8songs]},
+    {"Name": "Myo Pya La Yaung Tan Chin", "year":1996, "listened":4371, "image": "https://i.scdn.co/image/ab67616d0000b2735f40a9df186bbd7597d95f53", "songs":[...Album9songs]},
+    {"Name": "Win Yoe Sune Mee - Polar Flame", "year":1997, "listened":5479, "image": "https://i.scdn.co/image/ab67616d0000b2739de7a63da706368c28902cac", "songs":[...Album10songs]},
+    {"Name": "A May (Tho) A Kywin Mae Metta Taw Thi Chin - Mother (or) Owner Of Endless Love", "year":1997, "listened":3560, "image": "https://www.layeik.com/wp-content/uploads/edd/2017/06/Htoo-Eain-Thin-A-May-768x768.jpg", "songs":[...Album11songs]},
+    {"Name": "A Mhat Ta Ya Dan Ya Thi Chin Mya - Songs For Memorial Scars Of Life", "year":1999, "listened":8945, "image": "https://4.bp.blogspot.com/_qzszAXMm3BY/SA2Jmqx-4YI/AAAAAAAAAfc/ZAYE81R1St4/s400/HTOO+11.JPG", "songs":[...Album12songs]},
+    {"Name": "A Sein Yaung Yet Swe Myar - Green Dates ", "year":2001, "listened":7690, "image": "https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/59/77/c4/5977c443-41f2-af15-c9b9-b509711ebf8c/8720205504081.png/1200x1200bf-60.jpg", "songs":[...Album13songs]},
+    {"Name": "Ta Nae Sar A Lwe Myar - Mistakes of the Day", "year":2003, "listened":8469, "image": "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9b/c0/4e/9bc04e48-91a7-d6ea-4e9a-4d34566c5850/8720205504029.png/1200x1200bf-60.jpg", "songs":[...Album14songs]},
+    {"Name": "Chit Chin Ah Phyint - With Love", "year":2005, "listened":4267, "image": "https://th.bing.com/th/id/OIP.nyFIDh47jyFqh9aZ0v4EUAAAAA?rs=1&pid=ImgDetMain", "songs":[...Album15songs]},
+    {"Name": "Nhit Cho' Waing", "year":"(unreleased)", "listened":0, "image": "https://www.layeik.com/wp-content/uploads/Htoo-Eain-Thin-500x500.jpg", "songs":[...Album16songs]}
 ];
 
 export default titles;
